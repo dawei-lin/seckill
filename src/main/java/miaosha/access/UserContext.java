87f8ac5e06517d6f7a74b866e3894442e837974d
zimudashen <15626148176@163.com>
2018-08-25 15:46:47 +0800

    Initial commit

---
+++ src/main/java/miaosha/access/UserContext.java
@@ -0,0 +1,17 @@
+package miaosha.access;
+
+import miaosha.domain.MiaoshaUser;
+
+public class UserContext {
+	
+	private static ThreadLocal<MiaoshaUser> userHolder = new ThreadLocal<MiaoshaUser>();
+	
+	public static void setUser(MiaoshaUser user) {
+		userHolder.set(user);
+	}
+	
+	public static MiaoshaUser getUser() {
+		return userHolder.get();
+	}
+
+}
